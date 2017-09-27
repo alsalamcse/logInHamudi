1961e774cbdacc5725bcec068c88a95d4a533be4
user <muhammed.awad@outlook.com>
2017-09-27 13:10:58 +0300

    asas

--- app/src/main/java/com/awad/hamud/loginhamudi/REGISTER.java
+++ app/src/main/java/com/awad/hamud/loginhamudi/REGISTER.java
@@ -1,5 +1,6 @@
 package com.awad.hamud.loginhamudi;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.EditText;
 
 public class REGISTER extends AppCompatActivity
 {
+    private static final String PREF_FILE_NAME = ;
     private EditText etNUserName;
     private EditText etNPass;
     private EditText etEmail;
@@ -24,5 +26,13 @@ public class REGISTER extends AppCompatActivity
         etEmail = (EditText) findViewById(R.id.etEmail);
         btnSave = (Button) findViewById(R.id.btnSave);
         btnDelete = (Button) findViewById(R.id.btnDelete);
+    }
+}
+public onClick (View view)
+{
+    if (view==btnSave)
+    {
+        SharedPreferences prefs = getSharedPreferences(PREF_FILE_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
     }
 }
