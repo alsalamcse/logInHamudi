62465e0ccfdf7aea1607f4c8f7966e4450e48733
user <muhammed.awad@outlook.com>
2017-09-27 13:25:59 +0300

    asas

--- app/src/main/java/com/awad/hamud/loginhamudi/LOGIN.java
+++ app/src/main/java/com/awad/hamud/loginhamudi/LOGIN.java
@@ -1,13 +1,17 @@
 package com.awad.hamud.loginhamudi;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LOGIN extends AppCompatActivity
+public class LOGIN extends AppCompatActivity implements View.OnClickListener
 {
+    private static final String PREF_FILE_NAME = "MuhammedPref" ;
     private EditText etUName;
     private EditText etPass;
     private Button btnLogin;
@@ -22,4 +26,23 @@ public class LOGIN extends AppCompatActivity
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnSignup = (Button) findViewById(R.id.btnSignup);
     }
+
+
+public void onClick (View view){
+    if (view==btnLogin)
+    {
+    SharedPreferences prefs = getSharedPreferences(
+            PREF_FILE_NAME,MODE_PRIVATE);
+    String user = prefs.getString("user", null);
+    String passw = prefs.getString("passw" ,null);
+    if (user !=null || passw!=null)
+        Toast.makeText(this, "Succsfully logged", Toast.LENGTH_SHORT).show();
+        finish();
+
+        if (view==btnSignup){
+            Intent intent = new Intent(this,REGISTER.class);
+        }
+
+
+}
 }
--- app/src/main/java/com/awad/hamud/loginhamudi/REGISTER.java
+++ app/src/main/java/com/awad/hamud/loginhamudi/REGISTER.java
@@ -6,10 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class REGISTER extends AppCompatActivity
-{
-    private static final String PREF_FILE_NAME = ;
+public class REGISTER extends AppCompatActivity implements View.OnClickListener {
+    private static final String PREF_FILE_NAME = "MuhammedPref" ;
     private EditText etNUserName;
     private EditText etNPass;
     private EditText etEmail;
@@ -17,8 +17,7 @@ public class REGISTER extends AppCompatActivity
     private Button btnDelete;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         etNUserName = (EditText) findViewById(R.id.etNUserName);
@@ -27,12 +26,23 @@ public class REGISTER extends AppCompatActivity
         btnSave = (Button) findViewById(R.id.btnSave);
         btnDelete = (Button) findViewById(R.id.btnDelete);
     }
-}
-public onClick (View view)
-{
-    if (view==btnSave)
+
+    public void onClick(View view)
     {
-        SharedPreferences prefs = getSharedPreferences(PREF_FILE_NAME,MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
+        if (view == btnSave)
+        {
+            SharedPreferences prefs = getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("user", etNUserName.getText().toString());
+            editor.putString("passw", etNPass.getText().toString());
+            editor.putString("mail", etEmail.getText().toString());
+            editor.commit();
+
+            Toast.makeText(this, "Succsfelly SAVED", Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
+
     }
 }
+
